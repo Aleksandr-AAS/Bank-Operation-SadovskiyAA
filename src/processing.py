16d41502a8f6d3a2160dfa755b679b79ff0c63c4
Александр Садовский <sadovskiyaa@list.ru>
2025-10-16 17:37:19 +0300

    function in feature check black, flake8, mypy

--- src/processing.py
+++ src/processing.py
@@ -13,8 +13,17 @@
 # [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 # {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-def filter_by_state(my_list: list[dict], state="EXECUTED") -> list[dict]:
-    pass
+
+from datetime import datetime
+from typing import List, Dict
+
+
+def filter_by_state(my_list: List[Dict[str, str]], state: str = "EXECUTED") -> List[Dict[str, str]]:
+    """Функция принимает на вход список словарей с данными о банковских операциях и параметр state,
+    возвращает новый список, содержащий только те словари,
+    у которых ключ state содержит переданное в функцию значение"""
+    return [operation for operation in my_list if operation.get("state") == state]
+
 
 # Функция сортировка по дате
 # На входе [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -28,5 +37,7 @@ def filter_by_state(my_list: list[dict], state="EXECUTED") -> list[dict]:
 # {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 
-def sort_by_date (my_list: list[dict], reverse=True) -> list[dict]:
-    pass
+def sort_by_date(my_list: List[Dict[str, str]], reverse: bool = True) -> List[Dict[str, str]]:
+    """Функция принимает на вход список словарей и параметр порядка сортировки,
+    возвращает новый список, в котором исходные словари отсортированы по дате"""
+    return sorted(my_list, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
--- src/widget.py
+++ src/widget.py
@@ -40,6 +40,6 @@ def mask_account_card(my_str: str) -> str:
 
 
 def get_date(my_str: str) -> str:
-"""Функция которая выводит дату в формате ДД.ММ.ГГГГ"""
+    """Функция которая выводит дату в формате ДД.ММ.ГГГГ"""
     date_mask = my_str[8:10] + "." + my_str[5:7] + "." + my_str[0:4]
     return date_mask
