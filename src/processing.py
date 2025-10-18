ac8d0726e9ec1370b7b4c1d08db9dcbc0e838cc9
Александр Садовский <sadovskiyaa@list.ru>
2025-10-18 16:40:26 +0300

    add new branch  feature-1

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,3 @@
-# Функция фильтр по статусу
-#  На входе функции [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-# на выходе
-# # Выход функции со статусом по умолчанию 'EXECUTED'
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-#
-# # Выход функции, если вторым аргументов передано 'CANCELED'
-# [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
 
 from datetime import datetime
 from typing import List, Dict
@@ -24,17 +9,6 @@ def filter_by_state(my_list: List[Dict[str, str]], state: str = "EXECUTED") -> L
     у которых ключ state содержит переданное в функцию значение"""
     return [operation for operation in my_list if operation.get("state") == state]
 
-
-# Функция сортировка по дате
-# На входе [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-# {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-# Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-# {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 
 def sort_by_date(my_list: List[Dict[str, str]], reverse: bool = True) -> List[Dict[str, str]]:
