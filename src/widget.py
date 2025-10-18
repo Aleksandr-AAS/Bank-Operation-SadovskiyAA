c877af4f83ac8441f4370be31286c45d1f72eb62
Александр Садовский <sadovskiyaa@list.ru>
2025-10-18 09:33:48 +0300

    tests onemore

--- src/widget.py
+++ src/widget.py
@@ -43,3 +43,9 @@ def get_date(my_str: str) -> str:
     """Функция которая выводит дату в формате ДД.ММ.ГГГГ"""
     date_mask = my_str[8:10] + "." + my_str[5:7] + "." + my_str[0:4]
     return date_mask
+
+
+def get_pass(my_str: str) -> str:
+    date_mask = '1'
+    pass
+    return (date_mask)
