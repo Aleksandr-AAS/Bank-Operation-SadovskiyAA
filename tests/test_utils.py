b8dfcb375596729bd906587959f2b3a04ddfa171
Александр Садовский <sadovskiyaa@list.ru>
2025-11-25 17:08:53 +0300

    12.1 Библиотеки json, requests и datetime

---
+++ tests/test_utils.py
@@ -0,0 +1,47 @@
+import pytest
+import json
+from unittest.mock import patch
+from unittest.mock import mock_open
+from src.utils import get_list_dict_json
+
+
+@pytest.mark.parametrize(
+    "file_content,expected_output",
+    [
+        (
+            json.dumps(
+                [
+                    {
+                        "id": 441945886,
+                        "state": "EXECUTED",
+                        "date": "2019-08-26T10:50:58.294041",
+                        "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+                        "description": "Перевод организации",
+                        "from": "Maestro 1596837868705199",
+                        "to": "Счет 64686473678894779589",
+                    }
+                ]
+            ),
+            [
+                {
+                    "id": 441945886,
+                    "state": "EXECUTED",
+                    "date": "2019-08-26T10:50:58.294041",
+                    "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод организации",
+                    "from": "Maestro 1596837868705199",
+                    "to": "Счет 64686473678894779589",
+                }
+            ],
+        ),
+        ("", []),
+        ("{invalid json}", []),
+    ],
+)
+def test_get_list_dict_json_open_file(file_content: str, expected_output: list) -> None:
+
+    with patch("builtins.open", mock_open(read_data=file_content)) as mock_file:
+        result = get_list_dict_json("dummy_path.json")
+
+        assert result == expected_output
+        mock_file.assert_called_once_with("dummy_path.json", "r", encoding="utf-8")
